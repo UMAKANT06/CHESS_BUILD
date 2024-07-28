9bc3d95407bfbdd7d4a9adf27c8d766794936fa6
UMAKANT06 <114819136+UMAKANT06@users.noreply.github.com>
2024-07-28 17:27:33 +0530

    Add files via upload

---
+++ precache-manifest.23dd7616ca69b7e7155e396e293e3a8f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a2385df31c214ea0cbe0a57f92e547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37bac26514462284393e",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "4bc7362f4fc240cfee77",
+    "url": "/static/css/main.fdf61d55.chunk.css"
+  },
+  {
+    "revision": "37bac26514462284393e",
+    "url": "/static/js/2.6f69ec8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6f69ec8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc7362f4fc240cfee77",
+    "url": "/static/js/main.9d02f382.chunk.js"
+  },
+  {
+    "revision": "e4db7c41fb030e5ad0ee",
+    "url": "/static/js/runtime-main.ebb47c36.js"
+  },
+  {
+    "revision": "9d15b5ce28cb3ec22521eceb71b1196a",
+    "url": "/static/media/Black_Defeat.9d15b5ce.mp3"
+  },
+  {
+    "revision": "deba59ab3fb0c4a26ba4d1fd7baf9090",
+    "url": "/static/media/Capture.deba59ab.mp3"
+  },
+  {
+    "revision": "473ec93e278c7e75a04fc175dd954532",
+    "url": "/static/media/Check.473ec93e.mp3"
+  },
+  {
+    "revision": "ef2b8986efb7cbe118e6802643549ba4",
+    "url": "/static/media/Check_Flash.ef2b8986.mp3"
+  },
+  {
+    "revision": "cadfb9f5b38e3d302f95eed8402cadd1",
+    "url": "/static/media/Move.cadfb9f5.mp3"
+  },
+  {
+    "revision": "bc88a682415eace57d3614eb989dedbf",
+    "url": "/static/media/Stalemate.bc88a682.mp3"
+  },
+  {
+    "revision": "5e262fb139f481f321509307e212e5df",
+    "url": "/static/media/White_Defeat.5e262fb1.mp3"
+  },
+  {
+    "revision": "c933817caa35c1203144812fee6c15ca",
+    "url": "/static/media/black_bishop.c933817c.png"
+  },
+  {
+    "revision": "538e0d91c9fb2f4d2e8034f78bafe509",
+    "url": "/static/media/black_king.538e0d91.png"
+  },
+  {
+    "revision": "5eb634976444e1c53e473baaa923abc3",
+    "url": "/static/media/black_knight.5eb63497.png"
+  },
+  {
+    "revision": "aa1a10f58c7227a2dba7ca1433303d5c",
+    "url": "/static/media/black_pawn.aa1a10f5.png"
+  },
+  {
+    "revision": "62622c1c518ce81a37f8bbfc5fc86e59",
+    "url": "/static/media/black_queen.62622c1c.png"
+  },
+  {
+    "revision": "d6575a4508aa6fcbdf789e3b1941dbab",
+    "url": "/static/media/black_rook.d6575a45.png"
+  },
+  {
+    "revision": "cd6425561a0a3f942a0e1feac02bc4a5",
+    "url": "/static/media/white_bishop.cd642556.png"
+  },
+  {
+    "revision": "25d5241123da82320f4157d379e0f048",
+    "url": "/static/media/white_king.25d52411.png"
+  },
+  {
+    "revision": "4c558c096fd0c22f874a36eec66b70d0",
+    "url": "/static/media/white_knight.4c558c09.png"
+  },
+  {
+    "revision": "aaeeaa603f7918abcfb93e61d2283e7e",
+    "url": "/static/media/white_pawn.aaeeaa60.png"
+  },
+  {
+    "revision": "f232d65700a63df24ba1a40bdf4e39a4",
+    "url": "/static/media/white_queen.f232d657.png"
+  },
+  {
+    "revision": "b752eea145a92d9ea5b8680d6858e9ec",
+    "url": "/static/media/white_rook.b752eea1.png"
+  }
+]);
